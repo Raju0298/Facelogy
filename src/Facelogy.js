956d952fa197657d9f3b445833f6742d6c47f6d7
Raju0298 <rajutripathi0298@gmail.com>
2023-04-16 23:01:20 +0530

    Delete some unecessary files

--- src/Facelogy.js
+++ src/Facelogy.js
@@ -1,97 +1,82 @@
-import React, { useContext } from 'react'
-import { useEffect } from 'react';
-// import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
 import './css/Facebook.css'
 
+const Facelogy = (props) => {
+  const [data, setData] = useState()
+  useEffect(() => {
+    window.fbAsyncInit = function () {
+      window.FB.init({
+        appId: props.appId,
+        cookie: true,
+        xfbml: true,
+        version: "v16.0",
+      });
+      window.FB.AppEvents.logPageView();
+      window.FB.getLoginStatus(function (response) {
+        if (response.status === "connected") {
+          props.onLoginSuccess(response.authResponse);
+        }
+      });
+    };
 
-const mystyle= {
-  "background-color": "#3B5998",
-  "color": "white",
-  "margin-top": "10px",
-  "padding": "10px",
-  "border": "3px solid white",
-  "border-radius": "10px",
-  "width": "350px",
-  "display": "flex",
-  "align-items": "center",
-  "justify-content": "center"
+    (function (d, s, id) {
+      var js,fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v16.0";
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, "script", "facebook-jssdk");
+  }, [props.appId, props.onLoginSuccess, props.onLoginFailure]);
+
+
+  const callApi = (status, res) => {
+    const token = res.accessToken
+    const endpoint = "/me";
+    const method = "GET";
+    const params = {locale: 'en_US', fields: "id,first_name,last_name,email,picture", access_token: token};
+
+    window.FB.api(endpoint,method,params,
+        function(response) {
+        if (props.localStorage) {
+            localStorage.setItem('user', JSON.stringify(response));
+        }
+        if (props.handleResponse) {
+            props.handleResponse(response);
+        }
+        if (props.briefConsoleLog) {
+            console.log("ID: " + response.id);
+            console.log('First Name: ' + response.first_name);
+            console.log('Last Name: ' + response.last_name);
+            console.log("Email: " + response.email);
+            console.log("Image URL: " + response.picture);
+        }
+        props.onLoginSuccess(response, status);
+        // console.log(response);
+       });   
 }
 
-
-
-const Facelogy = (props) => {
-    
-    // const navigate = useNavigate()
-    // const [data, setData] = useState({})
-    useEffect(() => {
-        // const fbid = process.env.REACT_APP_FACEBOOKID
-        const fbid = props.appId
-        window.fbAsyncInit = function() {
-            window.fbLoaded = true
-            window.FB.init({
-                appId: fbid,
-                cookie: false, // Enable cookies to allow the server to access the session.
-                xfbml: true, // Parse social plugins on this webpage.
-                version: "v16.0", // Use this Graph API version for this call.
-              });
-
-            window.FB.AppEvents.logPageView();
-          };
-        
-        
-
-          (function(d, s, id){
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) {return;}
-            js = d.createElement(s); js.id = id;
-            js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v16.0";
-            fjs.parentNode.insertBefore(js, fjs);
-          }(document, 'script', 'facebook-jssdk'));                    
-    }, [])
-
-    
-    
-    const handleClick = () => { 
-        // console.log("HEre")
-        window.FB.login(function(response) {
-            if (response.authResponse) {
-            // var access_token =   window.FB.getAuthResponse()['accessToken'];
-
-            window.FB.getLoginStatus(function(response){
-                if(response.status==='connected'){
-                    console.log('Login authenticated');
-                }else{
-                    console.log('not authenticated');
-                }
-            })
-            console.log('Welcome!  Fetching your information.... ');
-            callApi()
-            setTimeout(() => {
-                // navigate(props.redirect)
-                window.location.href = props.redirect
-            }, 1000);
+  const handleLogin = () => {
+    window.FB.login(
+      function (response) {
+        if (response.authResponse) {
+          callApi(response.status, response.authResponse)
         } else {
-             console.log('User cancelled login or did not fully authorize.');
-            }
-        }, {scope: 'public_profile,email'});
-    }
+          props.onLoginFailure(response.status);
+        }
+      },
+      { scope: "public_profile,email" }
+    );
 
-    // const {data, setData} = useContext(ResponseContext)
-    
-    const callApi = () => {
-          window.FB.api('/me', {locale: 'en_US', fields: 'id,first_name,last_name,email,picture'},
-              function(response) {
-              //  setData(response)
-               localStorage.setItem("data", response)
-               console.log(response);
-             });   
-    }
+
+
+
+ 
+  };
 
   return (
-    <div>
-      <button className="fbtn" onClick={handleClick}>Login</button>
-    </div>
-  )
-}
+    <button className="fbtn" onClick={handleLogin}>Login with Facebook</button>
+  );
+};
 
 export default Facelogy;
--- src/index.js
+++ src/index.js
@@ -1,32 +1,4 @@
-import React, { createContext, useState } from 'react';
-// // import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import Facelogy from './Facelogy';
-
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import reportWebVitals from './reportWebVitals'
-// import App from './App';
-// export {default as FbLogin} from "./Facelogy";
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// const appId=""
-// const redirect=""
-// root.render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <div>
-//       <Routes>
-//           <Route exact path="/" element={<Facelogy appId="1129719461759606" redirect="/"/>}/>
-//       </Routes>
-//       </div>
-//     </BrowserRouter>
-//   </React.StrictMode>
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
 
 export default Facelogy;
